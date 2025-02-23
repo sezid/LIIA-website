7f546f31c9e6e857b9100360af01e25ec66edf99
sezid <bradsajid@gmail.com>
2025-02-24 00:20:04 +0600

    added job application

--- frontend/src/components/CareerApplication/CareerApplication.jsx
+++ frontend/src/components/CareerApplication/CareerApplication.jsx
@@ -59,7 +59,7 @@ const CareerApplication = ({ onClose }) => {
     }
   
     try {
-      const response = await fetch("http://localhost:5000/apply", {
+      const response = await fetch("https://liia-website.onrender.com/apply", {
         method: "POST",
         body: formDataToSend,
       });
